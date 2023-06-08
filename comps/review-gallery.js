aaa91474dbfcd7b2567e4d7fa977b0e9b6225efe
tgd_nWaA <bohdan.buchok@ukma.edu.ua>
2023-06-08 22:26:14 +0300

    galleries headings underlined with underline.scss

--- comps/review-gallery.js
+++ comps/review-gallery.js
@@ -1,21 +1,20 @@
-import Review from "./review"
+import Review from "./review";
 
 import s from "../styles/main/review-gallery.module.scss";
 
 const ReviewGallery = () => {
-    return (  
-        <div className={s.gallery}>
-            <h1>Наші відгуки</h1>
-            <div className={s['sub-gallery']}>
-                <Review/>
-                <Review/>
-                <Review/>
-                <Review/>
-                <Review/>
-            </div>
-            
-        </div>
-    );
-}
- 
+  return (
+    <div className={s.gallery}>
+      <h1 className={`${s.heading} ${s["underline-magical"]}`}>Наші відгуки</h1>
+      <div className={s["sub-gallery"]}>
+        <Review />
+        <Review />
+        <Review />
+        <Review />
+        <Review />
+      </div>
+    </div>
+  );
+};
+
 export default ReviewGallery;
